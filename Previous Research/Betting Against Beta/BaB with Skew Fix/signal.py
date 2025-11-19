62f773262cf7b2943faec5572c36203f63864dfa
Jai <78785071+jai-chau@users.noreply.github.com>
2025-11-18 18:13:17 -0700

    renamed folders and added placeholder for BaB writeup

---
+++ Previous Research/Betting Against Beta/BaB with Skew Fix/signal.py
@@ -0,0 +1,21 @@
+''' 
+
+Signal - Betting Against Beta
+Variation - Skew Adjustment 
+Data Required - Barra Predicted Beta (predbeta/predicted_beta/pred_beta/etc...)
+
+Explanation:
+We found that our distribution of betas is right-skewed while we want a normal distribution for our beta calculations.
+So, we run our betas through a rank transform and a reverse cdf to get our desired distribution.
+And since our signal is going short high beta stocks and long low beta stocks, we use (-1 * skewadjusted_predbeta) as our signal
+
+'''
+
+df = (
+    data.lazy()
+    .sort(["barrid", "date"])
+    # --- BAB = -predicted_beta ---
+    .with_columns([
+        (-pl.col("predicted_beta")).alias("bab")
+    ])
+)
