cb5bcb1fa70a69d503001ca7066e312350ee4bb7
Jai <78785071+jai-chau@users.noreply.github.com>
2025-11-18 18:20:29 -0700

    fixed code

--- Previous Research/Betting Against Beta/Classic BaB/signal.py
+++ Previous Research/Betting Against Beta/Classic BaB/signal.py
@@ -11,27 +11,15 @@ Since our signal is going short high beta stocks and long low beta stocks, we us
 '''
 
 import polars as pl 
-from scipy.stats import norm
-import numpy as np
 
 data = pl.DataFrame()
 
 # Start of Signal Code
-# We ran our 'predbeta' within each date and scaling to [0, 1].
-ranked_df = (
-    data.sort(["date", "predbeta"])
-    .with_columns(
-        (
-            (pl.col("predbeta").rank("average").over("date") - 1)
-            / (pl.col("predbeta").count().over("date") - 1)
-        ).alias("rank_scaled")
-    )
+df = (
+    data.lazy()
+    .sort(["barrid", "date"])
+    # --- BAB = -predicted_beta ---
+    .with_columns([
+        (-pl.col("predicted_beta")).alias("bab")
+    ])
 )
-
-# We then apply the inverse CDF of the standard normal distribution to remove skewness
-transformed_df = ranked_df.with_columns(
-    pl.col("rank_scaled").map_elements(lambda x: norm.ppf(np.clip(x, 1e-6, 1 - 1e-6)), return_dtype=pl.Float64).alias("noskew_predbeta")
-)
-
-# We then multiply by -1 to get the desired signal (Long low beta and short high beta)
-signals = transformed_df.with_columns(pl.col("noskew_predbeta") * -1).select(["date", "barrid", "noskew_predbeta"]).sort(["barrid", "date"])
